7de415b6652a12c7a20dee09e9bee9f8e807e3c4
marta-lopezgon <marta.lopezgon91@gmail.com>
2018-06-15 12:40:15 +0200

    inital commit with status test

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,11 @@
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+
+public class ApiTest {
+}
