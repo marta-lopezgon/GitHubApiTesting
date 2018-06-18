e22155c9ced1d03a6107504557f600eaf5144a7a
marta-lopezgon <marta.lopezgon91@gmail.com>
2018-06-18 11:31:35 +0200

    added test for id, name, full-name and owner status

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -8,25 +8,72 @@ public class ApiTest {
     public void check_ApiStatus() {
         given().
                 when().
-                get("https://api.github.com/repos/metrolab/SingleDateAndTimePicker").
+                get("https://api.myjson.com/bins/n8jcu").
                 then().
                 statusCode(200);
     }
+
+    //check the structure of the json for the given endpoint
+    //(check that all the keys are in the response: id, name, full name, owner, etc…)
     @Test
-    public void test_OwnerLogin() {
+    public void check_Id() {
         given().
                 when().
-                get("https://api.github.com/repos/metrolab/SingleDateAndTimePicker").
+                get("https://api.myjson.com/bins/n8jcu").
+                then().
+                assertThat().
+                body("",hasKey("id"));
+    }
+
+    @Test
+    public void check_Name() {
+        given().
+                when().
+                get("https://api.myjson.com/bins/n8jcu").
+                then().
+                assertThat().
+                body("", hasKey("name"));
+    }
+
+    @Test
+    public void check_FullName() {
+        given().
+                when().
+                get("https://api.myjson.com/bins/n8jcu").
+                then().
+                assertThat().
+                body("", hasKey("full_name"));
+    }
+
+    @Test
+    public void check_Owner() {
+        given().
+                when().
+                get("https://api.myjson.com/bins/n8jcu").
+                then().
+                assertThat().
+                body("", hasKey("owner"));
+    }
+
+    //check for the presence of the word “metrolab” in the field “owner->login”
+    @Test
+    public void check_OwnerLogin() {
+        given().
+                when().
+                get("https://api.myjson.com/bins/n8jcu").
                 then().
                 assertThat().
                 body("owner",hasEntry("login","metrolab"));
     }
+
+    //same test in a different and not as accurate way
     @Test
-    public void test_LoginHasMetrolab() {
+    public void test_ApiHasMetrolab() {
         given().
                 when().
-                get("https://api.github.com/repos/metrolab/SingleDateAndTimePicker").
+                get("https://api.myjson.com/bins/n8jcu").
                 then().
                 body(containsString("metrolab"));
     }
+
 }
